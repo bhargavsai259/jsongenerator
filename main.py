3a61ef076fd080f21a82cc6bb0a477856426aac3
bhargavsai259 <bhargavsaikumar259@gmail.com>
2025-12-11 22:50:04 +0530

    added fastapi

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from groq import Groq
 from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API running"}
+
+
+
+
+
 
