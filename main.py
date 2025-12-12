c06fbc0ae6b6b0c33270515a0cbd7d1bc457a772
bhargavsai259 <bhargavsaikumar259@gmail.com>
2025-12-12 23:14:02 +0530

    json geneartor agent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from groq import Groq
 from dotenv import load_dotenv
 import os
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -20,12 +21,19 @@ async def generate_json(request: PromptRequest):
         # Correct API call
         response = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
-            messages=[{"role": "user", "content": prompt_text}]
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a JSON generator. Always return ONLY valid JSON. No code blocks. No backticks."
+                },
+                {"role": "user", "content": prompt_text}
+            ]
         )
+
         # Extract AI-generated text
         generated_text = response.choices[0].message.content
-        s=generated_text
-        return {"response": generated_text}
+        s=json.loads(generated_text)
+        return s
 
     except Exception as e:
         return {"error": str(e)}
